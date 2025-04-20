bead5f922bce2533f37384983164ac28e68bb4c5
xdNecron <venomdushii@gmail.com>
2025-04-20 17:48:18 +0200

    Autopush 2025-04-20

--- autopush.sh
+++ autopush.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 
-DATE='%(%Y-%m-%d)T'
-
 git --git-dir=$HOME/.dotfiles/.git -C $HOME/.dotfiles add -A
-git --git-dir=$HOME/.dotfiles/.git -C $HOME/.dotfiles commit -m "Autopush $DATE"
+git --git-dir=$HOME/.dotfiles/.git -C $HOME/.dotfiles commit -m "Autopush $(date +%F)"
 git --git-dir=$HOME/.dotfiles/.git -C $HOME/.dotfiles push
