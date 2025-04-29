fd2ff034bf7cee4faf61f5d5b1e50173b4c551ee
xdNecron <venomdushii@gmail.com>
2025-04-29 18:00:01 +0200

    Autopush 2025-04-29

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("/home/popos/Documents/notes/tutorial.org")))
+ '(org-agenda-files
+   '("/home/popos/Documents/projects/javascript-academy/notes/notes.org"))
+ '(warning-suppress-types '((emacs) (emacs) (defvaralias) (lexical-binding))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
