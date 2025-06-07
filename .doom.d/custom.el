17877f96189b710b8ff7464c2d52e4923e431e4b
xdNecron <venomdushii@gmail.com>
2025-06-07 18:00:01 +0200

    Autopush 2025-06-07

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -19,4 +19,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#1a1b26" :foreground "#a9b1d6" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 135 :width normal :foundry "CTDB" :family "Fira Mono")))))
