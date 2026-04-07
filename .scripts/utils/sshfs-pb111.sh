b3b14735babd3c8bd9ae476c098545f536340912
xdNecron <venomdushii@gmail.com>
2026-04-07 23:45:54 +0200

    i3,other: betterlockscreen, singular bgimage src
    Swapped to betterlockscreen. Added singular ~/.bgimage file for unified
    access for all programs.

--- .scripts/utils/sshfs-pb111.sh
+++ .scripts/utils/sshfs-pb111.sh
@@ -9,7 +9,7 @@ REMOTE_DIR=aisa:/home/xeldunia/pb111
 if [ "$1" = "mount" ]; then
     sshfs -o auto_unmount $REMOTE_DIR $MOUNT_DIR
 elif [ "$1" = "umount" ]; then
-    fusermount -u $MOUNT_DIR
+    fusermount -zu $MOUNT_DIR
 else
     echo "$HELP_MSG"
     exit 1;
